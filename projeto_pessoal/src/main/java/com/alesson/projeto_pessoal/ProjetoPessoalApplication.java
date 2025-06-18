665c311be3fd7230308ed4c7bba142bd1aec7047
AlessonCarlos <alesson.carlos5@gmail.com>
2025-06-17 23:36:49 -0300

    Inicio e configuração do projeto

---
+++ projeto_pessoal/src/main/java/com/alesson/projeto_pessoal/ProjetoPessoalApplication.java
@@ -0,0 +1,13 @@
+package com.alesson.projeto_pessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPessoalApplication.class, args);
+	}
+
+}
---
+++ projeto_pessoal/src/test/java/com/alesson/projeto_pessoal/ProjetoPessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alesson.projeto_pessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
