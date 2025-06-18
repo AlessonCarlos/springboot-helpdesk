ba4e01e1fd241e1d79fe1434daa3ba59861c7625
AlessonCarlos <alesson.carlos5@gmail.com>
2025-06-18 16:15:16 -0300

    Anotações JPA para classes de dominio

--- projeto_pessoal/src/main/java/helpdesk/domain/Chamado.java
+++ projeto_pessoal/src/main/java/helpdesk/domain/Chamado.java
@@ -1,20 +1,45 @@
 package helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import org.apache.tomcat.util.http.parser.Priority;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import helpdesk.domain.enums.Status;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import helpdesk.domain.enums.Prioridade;
 
-public class Chamado {
+@Entity
+public class Chamado implements Serializable {
+    private static final long serialVersionUID = 1L; 
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataAbertura = LocalDate.now();
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataFechamento;
+
+    
     private Prioridade prioridade;
     private Status status;
     private String titulo;
     private String observacoes;
+
+    @ManyToOne
+    @JoinColumn(name ="tecnico_id")
     private Tecnico tecnico;
+
+    @ManyToOne
+    @JoinColumn(name ="cliente_id")
     private Cliente cliente;
     public Chamado() {
         super();
--- projeto_pessoal/src/main/java/helpdesk/domain/ProjetoPessoalApplication.java
+++ projeto_pessoal/src/main/java/helpdesk/domain/ProjetoPessoalApplication.java
@@ -1,4 +1,4 @@
-package com.alesson.projeto_pessoal;
+package helpdesk.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
